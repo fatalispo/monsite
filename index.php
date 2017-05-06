0303a4a7c226e77ab4bfcaa8a914cf5da14aa890
fatalispo <benjamin.dumay@hotmail.fr>
2017-05-06 20:33:46 +0200

    Ajout meta dans head

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="autor">
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<title>Bienvenue sur ma page</title>
+		<meta name="author" content="Benjamin Dumay" />
+
+	</head>
+
+	<body>
+
+	</body>
+</html>
