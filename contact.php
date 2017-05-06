3a0bada40d7402c4c76ede2334d7321f7a315000
fatalispo <benjamin.dumay@hotmail.fr>
2017-05-07 01:01:50 +0200

    Ajout du formulaire de contact : nom, prenom, email, objet, message

--- contact.php
+++ contact.php
@@ -12,5 +12,29 @@
 		
 		<?php include("menus.php");?>
 
+		<form method="POST" action="traitement_contact.php">
+			<legend>Formulaire de mise en relationS</legend>
+				<p>
+					<label for="nom">Nom</label>
+					<input type="text" name="nom" id="nom" placeholder="DUPONT">
+				</p>
+				<p>
+					<label for="prenom">Prénom</label>
+					<input type="text" name="nom" id="prenom" placeholder="Jean">
+				</p>
+				<p>
+					<label for="email">E-mail</label>
+					<input type="email" name="nom" id="email" placeholder="example@nimportequoi.ca">
+				</p>
+				<p>
+					<label for="objet">Objet :</label>
+					<input type="text" name="nom" id="objet" placeholder="Un site ...">
+				</p>
+				<textarea rows="20" cols="110" name="message" id="message" placeholder="..."></textarea><br />
+				<p>
+				<input type="submit" value="Envoyer" />
+				</p>
+		</form>
+
 	</body>
 </html>
