b5bbdb66a4fef2d8a2574cf6c49d6e375d006e3b
fatalispo <benjamin.dumay@hotmail.fr>
2017-05-07 00:13:27 +0200

    création du menu au format déroulant

--- menus.php
+++ menus.php
@@ -1,8 +1,26 @@
 <nav>
-	<ul>
-		<li><a href="index.php">Accueil</a></li>
-		<li><a href="cv.php">Curriculum Vitae</li>
-		<li><a href="services.php">Ce que je peux vous proposer</a></li>
+	<ul id="menu-deroulant">
+		<li><a href="index.php">Accueil</a>
+			<ul>
+				<li><a href="index.php#enbref">En bref</a></li>
+				<li><a href="index.php#demo">Démonstration</a></li>
+			</ul>
+		</li>
+		<li><a href="cv.php">Curriculum Vitae</a>
+			<ul>
+				<li><a href="cv.php#formations">Mes formations</a></li>
+				<li><a href="cv.php#exppro">Expériences professionnelles</a></li>
+				<li><a href="cv.php#knowledgeshobbies">Compétences et intérêts</a></li>
+			</ul>
+		</li>
+		<li><a href="services.php">Le deal</a>
+			<ul>
+				<li><a href="services.php#htmlcss">HTML/CSS</a></li>
+				<li><a href="services.php#php">PHP</a></li>
+				<li><a href="services.php#contenu">Contenu</a></li>
+				<li><a href="services.php#polyvalent">Polyvalent</a></li>
+			</ul>
+		</li>
 		<li><a href="contact.php">Me contacter</a></li>
 	</ul>
 </nav>
