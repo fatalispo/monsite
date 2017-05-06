92a89a595f761a5b83292464cdde7ba109afd8e4
fatalispo <benjamin.dumay@hotmail.fr>
2017-05-06 21:04:34 +0200

    ajout du menu et ses 4 pages

---
+++ menus.php
@@ -0,0 +1,8 @@
+<nav>
+	<ul>
+		<li><a href="index.php">Accueil</a></li>
+		<li><a href="cv.php">Curriculum Vitae</li>
+		<li><a href="services.php">Ce que je peux vous proposer</a></li>
+		<li><a href="contact.php">Me contacter</a></li>
+	</ul>
+</nav>
