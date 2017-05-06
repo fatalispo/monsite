e0e80e68b6e48904106c63325b8c35950a3fe4fc
fatalispo <benjamin.dumay@hotmail.fr>
2017-05-06 21:08:02 +0200

    creation des différentes pages du site

---
+++ contact.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<title>Bienvenue sur ma page</title>
+		<meta name="author" content="Benjamin Dumay"/>
+		<link rel="shortcut icon" type="image/x-icon" href="image/benjamindumay.png"/>
+	</head>
+
+	<body>
+		
+	</body>
+</html>
