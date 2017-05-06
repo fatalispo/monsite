0d070e726a264cfd1e3ba459606f13917949d70a
fatalispo <benjamin.dumay@hotmail.fr>
2017-05-07 00:11:07 +0200

    Création de la page CV + contenu + titres + ancrage

--- cv.php
+++ cv.php
@@ -1,14 +1,74 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 	<head>
+	    <meta name="author" content="Benjamin Dumay"/>
 		<meta charset="utf-8"/>
 		<link rel="stylesheet" type="text/css" href="style.css">
-		<title>Bienvenue sur ma page</title>
-		<meta name="author" content="Benjamin Dumay"/>
 		<link rel="shortcut icon" type="image/x-icon" href="image/benjamindumay.png"/>
+		<title>Mon parcours</title>
 	</head>
 
 	<body>
 		
+		<?php include("menus.php");?>
+		<br/>
+			<h1>Mon parcours</h1>
+				<h2 id="formations">Mes formations</h2>
+					<p>2016 – 2017 Débute une formation de développeur web avec le CNAM en ligne.</p>
+					<p>2016 – 2017 Certificat pour plusieurs MOOC développement web sur OpenClassroom.</p>
+					<p>2016 – 2017 Master 1 Science politique à l’Université de Versailles Saint-Quentin.</p>
+					<p>2013 – 2016 Licence Humanités à l’Université de Lorraine à METZ.</p>
+					<p>2012 – 2013 Formation continue présentiel Diplôme d'Accès aux Etudes Universitaires A orientation littéraire,(équivalence baccalauréat) à l'I.U.T. de Saint-Nazaire.</p>
+					<p>2012 Cycle préparatoire au DAEU A orientation littéraire par correspondance avec le CNED</p>
+					<p>2009 CAP Agent de Prévention et de Sécurité, Lycée Professionnel Blaise Pascal, SAINT JEAN D'ANGELY <i>Étude de cas juridiques, apprentissage du droit pénal dans le dessein de la bonne pratique du métier d’agent de sécurité.</i></p>
+		<br/>
+				<h2 id="exppro">Expériences professionnelles</h2>
+				<ul>2016
+					<li>Du 09/2016 au 01/2017, Chargé de mission Conseil Citoyen – Service civique, mairie de Guyancourt (78).</li>
+					<li>Du 05/2016 au 07/2016, Agent de sécurité, Securitas CHAURAY (79).</li>
+				</ul>
+				<ul>2014
+					<li>Du 10/2014 au 06/2016, assistant de professeur d’anglais, français et philosophie, lycée Cormontaigne METZ.</li>
+				</ul>
+				<ul>2013
+					<li>Du 07/2013 au 08/2013 Stagiaire archéologue lnstitut de Paléontologie Humaine de Paris, Mont Bégo (06).</li>
+				</ul>
+				<ul>2012 
+					<li>Du 07/2012 au 10/2012 Agent de prévention et de sécurité, ISP, chantier naval de SAINT-NAZAIRE.</li>
+					<li>Du 05/2012 au 07/2012 Agent de sécurité FUTUROSCOPE (86).</li>
+					<li>Du 01/2012 au 06/2012 Sapeur-Pompier Volontaire à OIRON (79).</li>
+				</ul>
+				<ul>2011 
+					<li>Du 09/2011 au 12/2011 Bénévole dans un ranch en Ontario CANADA.</li>
+					<li>Du 05/2011 au 07/2011 Contrôleur en qualité, Socab, à AIRVAULT (79).</li>
+					<li>Du 03/2011 au 04/2011 Agent de sécurité, Securitas, à SAUMUR (49).</li>
+				</ul>
+				<ul>2010
+					<li>2010 Du 01/2010 au 12/2010 Agent de Sécurité Incendie, Activ' Sécurité, Bordeaux.</li>
+				</ul>
+				<ul>2006
+					<li>2006 – 2007 Jeunes Sapeurs-Pompiers à THOUARS (79).</li>
+				</ul>
+		<br/>
+				<h2 id="knowledgeshobbies">Compétences et intérêts</h2>
+				<ul>Langues : 
+					<li>Anglais (intermédiaire) 4 mois en Ontario (CANADA) et intervenant dans cette langue auprès d’élèves dans une classe préparatoire ;</li>
+					<li>Russe (A2) compagne russe et 2 mois à Ekaterinbourg (RUSSIE).</li>
+				</ul>
+				<ul>Informatique :
+					<li>Création de diaporamas,</li>
+					<li>traitement de texte,</li>
+					<li>calendrier,</li> 
+					<li>publipostage,</li> 
+					<li>HTML5,</li> 
+					<li>CSS3,</li>
+					<li>Wordpress (bases),</li>
+					<li>Java (bases),</li> 
+					<li>PHP5</li>
+					<li>Git et GitHub.</li>
+				</ul>
+					<p>Loisirs : astronomie, collection de livres anciens, escrime artistique, footing, informatique, lecture, peinture, etc.</p>
+					<p>Pédagogie : Organiser et donner des leçons d’anglais pour des élèves en difficulté et des conseils de méthode pour rédiger une dissertation de philosophie.</p>
+					<p>Permis : A et B.</p>
 	</body>
 </html>
