2aae6307a6ac021a0a1bdc4d3c86d73bc1a25028
fatalispo <benjamin.dumay@hotmail.fr>
2017-05-07 00:14:25 +0200

    Création de la page service + contenu + titres + ancrage

--- services.php
+++ services.php
@@ -1,14 +1,48 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 	<head>
+	    <meta name="author" content="Benjamin Dumay"/>
 		<meta charset="utf-8"/>
 		<link rel="stylesheet" type="text/css" href="style.css">
-		<title>Bienvenue sur ma page</title>
-		<meta name="author" content="Benjamin Dumay"/>
 		<link rel="shortcut icon" type="image/x-icon" href="image/benjamindumay.png"/>
+		<title>Services</title>
 	</head>
 
 	<body>
+
+		<?php include("menus.php");?>
+		<br/>
+			<h1>Ce que je peux vous proposer</h1>
+				<h2 id="htmlcss">HTML/CSS</h2>
+					<p>Ce site est une illustration de l'état de mes connaissances en HTML/CSS actuel. Ayant commencé à coder seulement à la fin du mois de février 2017, on peut me qualifier de débutant, c'est tout à fait légitime. Ce n'est pas réducteur, puisque tout le monde est passé par cette étape.</p>
+					<p>Cependant nous possédons tous un niveau de progression différent et ayant récemment débuté dans le développement, je trouve mon niveau plutôt démonstrateur d'une grande motivation.</p>
+					<p>Une grande motivation qui a pu m'amener à vouloir creuser dans le développement web et à découvrir de nouveaux langages.</p>
+					<p>La motivation est donc mon moteur et c'est notamment grâce à elle que vous pouvez vous trouver sur cette page en train de lire ces lignes.</p>
+					<p>Ce que je vous propose, est simple. Si vous désirez un coup de main bénévole pour votre site web (ponctuellement) rendez-vous à la page contact</p>
+		<br/>
+				<h2 id="php">PHP</h2>
+					<p>PHP est sûrement après HTML et CSS le langage que je maîtrise le mieux. J'approfondis actuellement mes connaissances dans ce langages aux ramifications étendues. Effectivement les possibilités avec PHP sont immenses et les conditions comptent parmi mes jeux préférées</p>
+					<p>Cela peut paraître étrange, voire suspect, mais cela est franchement plaisant de pouvoir ordonner à sa machine 'Fait-ci ! Fait-ça !' et que celle-ci obéisse au doigt et à l'oeil ! (contrairement à mon chien, une vraie petite teigne ce Jacob! Je m'égare...)</p>
+					<p>Pour PHP je peux actuellement pas vraiment vous aider mais participer à des projets me ferait progresser puisque j'apprendrais de vous. Ce que je vous propose donc c'est de m'apprendre. Et oui l'échange n'est pas à sens unique sinon ce n'est pas un échange...</p>
+		<br/>
+				<h2 id="contenu">Contenu</h2>
+					<p>Pour le contenu, je pense pouvoir écrire correctement en français. Oui, c'est dans mes cordes à votre plus grand étonnement. Après avoir aidé des lycéens pour qu'ils puissent rédiger correctement leurs copies et corriger plusieurs dizaines de copies, je pense pouvoir vous proposer un contenu de qualité.</p>
+					<p>N'étant pas aussi bon écrivain que Léon Tolstoï, en dépit de mes trois années en Humanités à l'Université de Lorraine, je ne peux vous proposez que du Benjamin Dumay.</p>
+					<p>Humanités ? Et c'est quoi le rapport ? Humanitaire ... Tolstoï ?</p>
+					<p>Il divague le jeune homme...</p>
+					<p>Non, je ne divague pas. Sachez, très cher, que les Humanités furent longtemps les études qu'entrepennaient tous étudiants dignent de ce nom !</p>
+					<p>Effectivement on y apprenait le latin, le grec ancien, les lettres, les sciences humaines... Et oui, on y apprenait l'humanité.</p>
+					<p>Tu es donc un spécialiste de l'humanité ?</p>
+					<p>On va plutôt dire, que je possède, grâce à cette modeste licence, une culture générale me permettant d'avoir une vision globale sur l'Homme depuis qu'il est Homme.</p>
+					<p>Et c'est quoi le rapport avec du contenu ?</p>
+					<p>C'est pourtant évidant ! Il m'est possible de rebondir sur tout, je peux donc m'adapter au contenu que vous souhaitez et l'enrichir de mes quelques connaissances.</p>
+					<p>Pas mal, non ?</p>  
+		<br/>
+				<h2 id="polyvalent">Polyvalent</h2>
+					<p>Et enfin, le clou du spectacle !</p>
+					<p>Je suis un homme polyvalent ! </p>
+					<p>Et je vous propose donc mes services d'homme polyvalent !</p>
+					<p>Sécurité, politique et informatique sont les trois domaines dans lesquels j'ai déjà travaillé (bon je triche un peu pour l'informatique car je n'ai jamais été payé puisque je créé pour moi-même, je ne vais tout de même pas me payer...). Je suis donc ouvert à des propositions en lien avec ces domaines, bien que j'ai une grosse préférence pour l'informatique.</p> 
 		
 	</body>
 </html>
