0a508554f90be58ec3880d8d0ccc323e831ac26a
fatalispo <benjamin.dumay@hotmail.fr>
2017-05-07 00:12:38 +0200

    Création de la page dacceuil
    exit
    end
    *
    !
    <
    >
    !

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+	    <meta name="author" content="Benjamin Dumay"/>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="shortcut icon" type="image/x-icon" href="image/benjamindumay.png"/>
+		<title>Bienvenue</title>
+	</head>
+
+	<body>
+
+		<?php include("menus.php");?>
+		
+	</body>
+</html>
