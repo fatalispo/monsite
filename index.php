2e80f36ba3f3184ecde6dd62bed6ccdc291c3807
fatalispo <benjamin.dumay@hotmail.fr>
2017-05-07 12:51:50 +0200

    Création texte+titres+ancrages+insertion image

--- index.php
+++ index.php
@@ -9,8 +9,27 @@
 	</head>
 
 	<body>
-
 		<?php include("menus.php");?>
+			<img src="image/clavier.jpg">
+		<br/>
+				<h2 id="enbref">En bref</h2>
+					<p>Ce site sert avant tout,à parler un peu de moi et à vous montrer ce que je sais faire. C'est une simple vitrine.</p>
+					<p>Et l'objet de cette vitrine c'est moi. Moi qui ne suis pas vendeur, je me met pourtant en vente. Non je ne parle pas de prostitution... Tout de même.</p>
+					<p>Je met juste mes connaissances à votre disposition et la réalisation sur laquelle vous êtes est une illustration de ce que je peux vous proposer.</p>
+				<br/>
+				<h2 id="demo">Démonstration</h2>
+					<center><img src="image/exempletravail.png"></center>
+					<p>Ce site, à lui seul, est une vitrine je n'ai donc pas besoin de vous faire de démonstration. Cependant je tenais à joindre une petite image qui vous présente brièvement mon environnement de travail.</p>
+					<ul>Environnement de travail :
+						<li>Ordinateur portable : DELL XPS17 I7.</li>
+						<li>Système d'exploitation : Linux Ubuntu.</li>
+						<li>Internet : Free 4MO débit descendant et 0.50MO en montant.</li>
+						<li>Editeur de texte : Sublime Text et/ou Emacs.</li>
+						<li>Versionnage : Git (via la console) et GitHub.</li>
+						<li>Retouche image : GIMP</li>
+						<li>Apprentissage du développement : Open Classroom, CNAM, Sololearn.</li>
+					</ul>
+
 		
 	</body>
 </html>
