cc13ed353d36fcfd6b7b58c11d3d6b9c07ee420d
fatalispo <benjamin.dumay@hotmail.fr>
2017-05-08 16:33:37 +0200

    ajout des commentaires

--- index.php
+++ index.php
@@ -10,14 +10,14 @@
 
 	<body>
 		<?php include("menus.php");?>
-			<center><img src="image/exempletravail.png" height="550px" width="100%"></center>
+			<img src="image/exempletravail.png" alt="Un exemple de codage" height="550px" width="100%" align="center"><!--Image créée via GIMP donc c'est ma propre source-->
 		<br/>
-				<h2 id="enbref">En bref</h2>
+				<h2 id="enbref">En bref</h2><!--Ancrage pour le menu-->
 					<p>Ce site sert avant tout,à parler un peu de moi et à vous montrer ce que je sais faire. C'est une simple vitrine.</p>
 					<p>Et l'objet de cette vitrine c'est moi. Moi qui ne suis pas vendeur, je me met pourtant en vente. Non je ne parle pas de prostitution... Tout de même.</p>
 					<p>Je met juste mes connaissances à votre disposition et la réalisation sur laquelle vous êtes est une illustration de ce que je peux vous proposer.</p>
 				<br/>
-				<h2 id="demo">Démonstration</h2>
+				<h2 id="envi">Environnement</h2><!--Ancrage pour le menu-->
 					
 					<p>Ce site, à lui seul, est une vitrine je n'ai donc pas besoin de vous faire de démonstration. Cependant je tenais à joindre la petite image ci-dessus qui vous présente brièvement mon travail en cours. Ci-dessous vous trouverez plus de détails sur mes outils de travail.</p>
 					<ul>Environnement de travail :
@@ -27,7 +27,7 @@
 						<li>Editeur de texte : Sublime Text et/ou Emacs.</li>
 						<li>Versionnage : Git (via la console) et GitHub.</li>
 						<li>Retouche image : GIMP</li>
-						<li>Apprentissage du développement : Open Classroom, CNAM, Sololearn.</li>
+						<li>Apprentissage du développement : Open Classroom, CNAM, Sololearn et Frogweb.</li>
 					</ul>
 
 		
