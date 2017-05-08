0b1f0aa81db6128aa9f30b19c829ebc2cba7790d
fatalispo <benjamin.dumay@hotmail.fr>
2017-05-08 12:16:16 +0200

    suppression image de clavier et remplacement par image exempletravail

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
 	<body>
 		<?php include("menus.php");?>
-			<img src="image/clavier.jpg">
+			<center><img src="image/exempletravail.png" height="350px" width="100%"></center>
 		<br/>
 				<h2 id="enbref">En bref</h2>
 					<p>Ce site sert avant tout,à parler un peu de moi et à vous montrer ce que je sais faire. C'est une simple vitrine.</p>
@@ -18,7 +18,7 @@
 					<p>Je met juste mes connaissances à votre disposition et la réalisation sur laquelle vous êtes est une illustration de ce que je peux vous proposer.</p>
 				<br/>
 				<h2 id="demo">Démonstration</h2>
-					<center><img src="image/exempletravail.png"></center>
+					
 					<p>Ce site, à lui seul, est une vitrine je n'ai donc pas besoin de vous faire de démonstration. Cependant je tenais à joindre une petite image qui vous présente brièvement mon environnement de travail.</p>
 					<ul>Environnement de travail :
 						<li>Ordinateur portable : DELL XPS17 I7.</li>
