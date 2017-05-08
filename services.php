da79e2ab8751967b48958b8096ca406959924ec9
fatalispo <benjamin.dumay@hotmail.fr>
2017-05-08 16:32:15 +0200

    ajout des commentaires et sources pour les images

--- services.php
+++ services.php
@@ -12,20 +12,24 @@
 
 		<?php include("menus.php");?>
 		<br/>
-			<h1>Ce que je peux vous proposer</h1>
-				<h2 id="htmlcss">HTML/CSS</h2>
+			<h1><center>Ce que je peux vous proposer</center></h1>
+		<br/>
+				<h2 id="htmlcss"><und>HTML/CSS</und></h2><!--Ancrage pour le menu source de l'image https://c1.staticflickr.com/6/5214/5383794499_f6e43fd12f_z.jpg-->
+				<img src="image/html5.jpg" alt="logo HTML5" align="right" width="200px" height="200px">
+
 					<p>Ce site est une illustration de l'état de mes connaissances en HTML/CSS actuel. Ayant commencé à coder seulement à la fin du mois de février 2017, on peut me qualifier de débutant, c'est tout à fait légitime. Ce n'est pas réducteur, puisque tout le monde est passé par cette étape.</p>
 					<p>Cependant nous possédons tous un niveau de progression différent et ayant récemment débuté dans le développement, je trouve mon niveau plutôt démonstrateur d'une grande motivation.</p>
 					<p>Une grande motivation qui a pu m'amener à vouloir creuser dans le développement web et à découvrir de nouveaux langages.</p>
 					<p>La motivation est donc mon moteur et c'est notamment grâce à elle que vous pouvez vous trouver sur cette page en train de lire ces lignes.</p>
 					<p>Ce que je vous propose, est simple. Si vous désirez un coup de main bénévole pour votre site web (ponctuellement) rendez-vous à la page contact</p>
 		<br/>
-				<h2 id="php">PHP</h2>
+				<h2 id="php"><und>PHP</und></h2><!--Ancrage pour le menu source de l'image https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/1280px-PHP-logo.svg.png-->
+				<img src="image/PHPlogo.png" alt="logo PHP" align="right" width="400px" height="200px">
 					<p>PHP est sûrement après HTML et CSS le langage que je maîtrise le mieux. J'approfondis actuellement mes connaissances dans ce langages aux ramifications étendues. Effectivement les possibilités avec PHP sont immenses et les conditions comptent parmi mes jeux préférées</p>
 					<p>Cela peut paraître étrange, voire suspect, mais cela est franchement plaisant de pouvoir ordonner à sa machine 'Fait-ci ! Fait-ça !' et que celle-ci obéisse au doigt et à l'oeil ! (contrairement à mon chien, une vraie petite teigne ce Jacob! Je m'égare...)</p>
 					<p>Pour PHP je peux actuellement pas vraiment vous aider mais participer à des projets me ferait progresser puisque j'apprendrais de vous. Ce que je vous propose donc c'est de m'apprendre. Et oui l'échange n'est pas à sens unique sinon ce n'est pas un échange...</p>
 		<br/>
-				<h2 id="contenu">Contenu</h2>
+				<h2 id="contenu">Contenu</h2><!--Ancrage pour le menu-->
 					<p>Pour le contenu, je pense pouvoir écrire correctement en français. Oui, c'est dans mes cordes à votre plus grand étonnement. Après avoir aidé des lycéens pour qu'ils puissent rédiger correctement leurs copies et corriger plusieurs dizaines de copies, je pense pouvoir vous proposer un contenu de qualité.</p>
 					<p>N'étant pas aussi bon écrivain que Léon Tolstoï, en dépit de mes trois années en Humanités à l'Université de Lorraine, je ne peux vous proposez que du Benjamin Dumay.</p>
 					<p>Humanités ? Et c'est quoi le rapport ? Humanitaire ... Tolstoï ?</p>
@@ -33,12 +37,12 @@
 					<p>Non, je ne divague pas. Sachez, très cher, que les Humanités furent longtemps les études qu'entrepennaient tous étudiants dignent de ce nom !</p>
 					<p>Effectivement on y apprenait le latin, le grec ancien, les lettres, les sciences humaines... Et oui, on y apprenait l'humanité.</p>
 					<p>Tu es donc un spécialiste de l'humanité ?</p>
-					<p>On va plutôt dire, que je possède, grâce à cette modeste licence, une culture générale me permettant d'avoir une vision globale sur l'Homme depuis qu'il est Homme.</p>
+					<p>On va plutôt dire, que je possède, grâce à cette modeste licence, une culture générale me permettant d'avoir une vision globale de l'Homme depuis qu'il est Homme.</p>
 					<p>Et c'est quoi le rapport avec du contenu ?</p>
-					<p>C'est pourtant évidant ! Il m'est possible de rebondir sur tout, je peux donc m'adapter au contenu que vous souhaitez et l'enrichir de mes quelques connaissances.</p>
+					<p>C'est pourtant évidant ! Il m'est, grâce à ces connaissances, possible de rebondir sur tout, je peux donc m'adapter au contenu que vous souhaitez et l'enrichir de mes quelques connaissances.</p>
 					<p>Pas mal, non ?</p>  
 		<br/>
-				<h2 id="polyvalent">Polyvalent</h2>
+				<h2 id="polyvalent">Polyvalent</h2><!--Ancrage pour le menu-->
 					<p>Et enfin, le clou du spectacle !</p>
 					<p>Je suis un homme polyvalent ! </p>
 					<p>Et je vous propose donc mes services d'homme polyvalent !</p>
