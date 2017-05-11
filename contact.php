c32eaa36eb45cd9f6b47f49456bdd25f9feb8f4b
fatalispo <benjamin.dumay@hotmail.fr>
2017-05-11 22:42:26 +0200

    redirection vers la boite mail protonmail

--- contact.php
+++ contact.php
@@ -12,7 +12,7 @@
 		
 		<?php include("menus.php");?>
 
-		<form method="POST" action="traitement_contact.php">
+		<form method="post" action="mailto:benjamindumay@protonmail.com" name="contact"> <!--envoi du formulaire primaire et incomplet car apprentissage php en cours-->
 		<br/>
 			<legend><center>Formulaire de mise en relation</center></legend>
 		<br/>
@@ -20,16 +20,16 @@
 					<input type="text" name="nom" id="nom" placeholder="Nom">
 				</p>
 				<p>
-					<input type="text" name="nom" id="prenom" placeholder="Prénom">
+					<input type="text" name="prenom" id="prenom" placeholder="Prénom">
 				</p>
 				<p>
-					<input type="email" name="nom" id="email" placeholder="adresse@email.com">
+					<input type="email" name="emeil" id="email" placeholder="adresse@email.com">
 				</p>
 				<p>
-					<input type="text" name="nom" id="objet" placeholder="Objet">
+					<input type="text" name="objet" id="objet" placeholder="Objet">
 				</p>
 		<br/>
-				<textarea rows="30" cols="210" id="zonetexte" placeholder="Ecrivez votre message ici"></textarea><br />
+				<textarea rows="30" cols="210" name="message" id="zonetexte" placeholder="Ecrivez votre message ici"></textarea><br />
 				<p><center>
 				<input type="reset" name="reset" value="Réinitialiser">
 				<input type="submit" name="envoyer" value="Envoyer" />
