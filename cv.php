3c1d37352931ad46a1c57eba831129340aff52cf
fatalispo <benjamin.dumay@hotmail.fr>
2017-05-11 22:43:09 +0200

    mise à jour

--- cv.php
+++ cv.php
@@ -21,12 +21,11 @@
 					<p>2013 – 2016 Licence Humanités à l’Université de Lorraine à METZ.</p>
 					<p>2012 – 2013 Formation continue présentiel Diplôme d'Accès aux Etudes Universitaires A orientation littéraire,(équivalence baccalauréat) à l'I.U.T. de Saint-Nazaire.</p>
 					<p>2012 Cycle préparatoire au DAEU A orientation littéraire par correspondance avec le CNED</p>
-					<p>2009 CAP Agent de Prévention et de Sécurité, Lycée Professionnel Blaise Pascal, SAINT JEAN D'ANGELY <i>Étude de cas juridiques, apprentissage du droit pénal dans le dessein de la bonne pratique du métier d’agent de sécurité.</i></p>
+					<p>2009 CAP Agent de Prévention et de Sécurité, Lycée Professionnel Blaise Pascal, SAINT JEAN D'ANGELY</p>
 		<br/>
 				<h2 id="exppro">Expériences professionnelles</h2><!--Ancrage pour le menu-->
-				<ul>2016
+				<ul>2016 - 2017
 					<li>Du 09/2016 au 01/2017, Chargé de mission Conseil Citoyen – Service civique, mairie de Guyancourt (78).</li>
-					<li>Du 05/2016 au 07/2016, Agent de sécurité, Securitas CHAURAY (79).</li>
 				</ul>
 				<ul>2014
 					<li>Du 10/2014 au 06/2016, assistant de professeur d’anglais, français et philosophie, lycée Cormontaigne METZ.</li>
@@ -34,21 +33,11 @@
 				<ul>2013
 					<li>Du 07/2013 au 08/2013 Stagiaire archéologue lnstitut de Paléontologie Humaine de Paris, Mont Bégo (06).</li>
 				</ul>
-				<ul>2012 
-					<li>Du 07/2012 au 10/2012 Agent de prévention et de sécurité, ISP, chantier naval de SAINT-NAZAIRE.</li>
-					<li>Du 05/2012 au 07/2012 Agent de sécurité FUTUROSCOPE (86).</li>
-					<li>Du 01/2012 au 06/2012 Sapeur-Pompier Volontaire à OIRON (79).</li>
-				</ul>
-				<ul>2011 
-					<li>Du 09/2011 au 12/2011 Bénévole dans un ranch en Ontario CANADA.</li>
-					<li>Du 05/2011 au 07/2011 Contrôleur en qualité, Socab, à AIRVAULT (79).</li>
-					<li>Du 03/2011 au 04/2011 Agent de sécurité, Securitas, à SAUMUR (49).</li>
-				</ul>
-				<ul>2010
-					<li>2010 Du 01/2010 au 12/2010 Agent de Sécurité Incendie, Activ' Sécurité, Bordeaux.</li>
+				<ul>2012 - 2011 - 2010
+					<li>Agent de sécurité (Bordeaux, Saint-Nazaire, Niort) et sapeurs-pompier volontaire.</li>
 				</ul>
 				<ul>2006
-					<li>2006 – 2007 Jeunes Sapeurs-Pompiers à THOUARS (79).</li>
+					<li>2006 - 2007 Jeunes Sapeurs-Pompiers THOUARS (79).</li>
 				</ul>
 		<br/>
 				<h2 id="knowledgeshobbies">Compétences et intérêts</h2><!--Ancrage pour le menu-->
@@ -66,6 +55,7 @@
 					<li>Wordpress (bases),</li>
 					<li>Java (bases),</li> 
 					<li>PHP5</li>
+					<li>JavaScript (bases)</li>
 					<li>Git et GitHub.</li>
 				</ul>
 					<p>Loisirs : astronomie, collection de livres anciens, escrime artistique, footing, informatique, lecture, peinture, etc.</p>
